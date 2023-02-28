1be60dc4dc04830f60362a0f6f2a1e7ad2a88d87
Polina Limonova <polina98_08@mail.ru>
2023-02-28 12:09:46 +0300

    slider tasks 1-2

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ BringItUp_study_project
 Educational project (OOP style)
 
 Tasks:
+
+Used:
+
+1. babel
+2. Gulp
+3. Webpack
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import Slider from "./modules/Slider";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const slider1 = new Slider(".page", ".next");
+  slider1.render();
+});
